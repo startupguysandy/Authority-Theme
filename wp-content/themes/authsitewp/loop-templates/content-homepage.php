8b6b19584e5e8db778c12b52942a4cf331472620
Andy Bradbury <andy@thestartupguys.co.uk>
2019-11-11 20:04:58 +0000

    implement code to pull posts into homepage

--- wp-content/themes/authsitewp/loop-templates/content-homepage.php
+++ wp-content/themes/authsitewp/loop-templates/content-homepage.php
@@ -14,26 +14,47 @@ defined( 'ABSPATH' ) || exit;
             <!-- start #primary -->
             <div class="col-md-6 offset-md-1 content-area" id="primary">
                 <main class="site-main" id="main">
-                    <article class="post-2 page type-page status-publish hentry" id="post-2">
-
                         <?php $the_query = new WP_Query( array( // the query
                             'posts_per_page' => 3,
                         ));	?>
 
                         <?php if ( $the_query->have_posts() ) : ?>
                             <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                                <header class="entry-header">
-                                    <h1 class="entry-title"><?php the_title(); ?></h1>
-                                </header>
-                                <div class="entry-content">
-                                    <?php the_excerpt(); ?>
-                                </div><!-- .entry-content -->
+                                <article <?php post_class('post-content'); ?> id="post-<?php the_ID(); ?>">
+                                    <div class="row">
+	                                    <?php if (has_post_thumbnail() ) : ?>
+                                            <div class="col-md-12 post-thumbnail-wrap">
+                                                <a href="<?php echo get_permalink(); ?>">
+                                                    <div class="post-thumbnail" style="background-image: url('<?php echo wp_get_attachment_url(get_post_thumbnail_id(get_the_ID())); ?>')"></div>
+                                                </a>
+                                            </div>
+                                        <?php endif; ?>
+
+                                        <div class="col-md-12">
+                                            <div class="blog-feed-content">
+                                                <header class="entry-header">
+                                                    <h2 class="entry-title">
+                                                        <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
+                                                    </h2>
+                                                    <div class="entry-meta">
+                                                        <h5 class="entry-date"><time class="entry-date" datetime="<?php echo get_the_date(); ?>"><?php the_date(); ?></time>
+                                                        </h5>
+                                                    </div><!-- .entry-meta -->
+                                                </header><!-- .entry-header -->
+                                                <div class="entry-summary">
+                                                    <a class="text-decoration-none" href="<?php echo get_permalink(); ?>">
+	                                                    <?php the_excerpt(); ?>
+                                                    </a>
+                                                </div><!-- .entry-summary -->
+                                            </div>
+                                        </div>
+                                    </div>
+                                </article><!-- #post-## -->
                             <?php endwhile; ?>
-                            <?php wp_reset_postdata(); ?>
+                            <?php echo wp_reset_postdata(); ?>
                         <?php else : ?>
                             <p><?php __('No News'); ?></p>
                         <?php endif; ?>
-                    </article><!-- #post-## -->
                 </main><!-- #main -->
             </div><!-- closing #primary -->
 

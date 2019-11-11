66bbd04beb845335d8a10036fa16dc7d3c660bfa
Andy Bradbury <andy@thestartupguys.co.uk>
2019-11-11 01:44:20 +0000

    tidy up header php file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Authority Theme
+Uses UnderStrap theme which makes use of Gulp, navigate to the theme folder and run:
+gulp watch
+
+Documentation:
+https://understrap.github.io/
+
+More info:
+https://usdocu.tarthemes.com/docs/0.8.2/development/available-gulp-tasks/
+https://usdocu.tarthemes.com/docs/0.8.2/development/developing-with-npm-and-gulp/
--- wp-content/themes/authsitewp/header.php
+++ wp-content/themes/authsitewp/header.php
@@ -25,56 +25,67 @@ $container = get_theme_mod( 'understrap_container_type' );
 <?php do_action( 'wp_body_open' ); ?>
 <div class="site" id="page">
 
-	<!-- ******************* The Navbar Area ******************* -->
-	<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
+    <!-- ******************* The Hero Banner Container ******************* -->
+    <div class="wrapper homepage-hero-banner" id="wrapper-hero" itemscope itemtype="http://schema.org/WebSite">
 
-		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
+	    <!-- ******************* The Navbar Area ******************* -->
+        <div class="container" id="wrapper-navbar">
+            <div class="row justify-content-center">
+                <div class="col-md-10">
 
-		<nav class="navbar navbar-expand-md navbar-dark bg-primary">
+                    <nav class="navbar navbar-expand-md navbar-dark bg-primary">
 
-		<?php if ( 'container' == $container ) : ?>
-			<div class="container">
-		<?php endif; ?>
+                        <!-- .container starts -->
+		                <?php if ( 'container' == $container ) : ?>
+                            <div class="container">
+                        <?php endif; ?>
 
-					<!-- Your site title as branding in the menu -->
-					<?php if ( ! has_custom_logo() ) { ?>
+                            <!-- Your site title as branding in the menu -->
+                            <!-- start custom logo -->
+                            <?php if ( ! has_custom_logo() ) { ?>
+                                <?php if ( is_front_page() && is_home() ) : ?>
+                                    <h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+                                <?php else : ?>
+                                    <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+                            <?php endif; ?>
 
-						<?php if ( is_front_page() && is_home() ) : ?>
+                            <?php } else {
+                                the_custom_logo();
+                            } ?>
+                            <!-- end custom logo -->
 
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
 
-						<?php else : ?>
+                                <!-- The WordPress Menu goes here -->
+                                <?php wp_nav_menu(
+                                    array(
+                                        'theme_location'  => 'primary',
+                                        'container_class' => 'collapse navbar-collapse',
+                                        'container_id'    => 'navbarNavDropdown',
+                                        'menu_class'      => 'navbar-nav ml-auto',
+                                        'fallback_cb'     => '',
+                                        'menu_id'         => 'main-menu',
+                                        'depth'           => 2,
+                                        'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+                                    )
+                                ); ?>
 
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+			                <?php if ( 'container' == $container ) : ?>
+                                </div>
+                            <?php endif; ?>
+                            <!-- .container ends -->
 
-						<?php endif; ?>
+                    </nav><!-- .site-navigation -->
+                </div>
+            </div>
 
+        </div><!-- #wrapper-navbar end -->
 
-					<?php } else {
-						the_custom_logo();
-					} ?><!-- end custom logo -->
+        <!-- ******************* The Hero Info ******************* -->
+        
 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-					<span class="navbar-toggler-icon"></span>
-				</button>
+        <!-- #hero-info end -->
 
-				<!-- The WordPress Menu goes here -->
-				<?php wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav ml-auto',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'depth'           => 2,
-						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-					)
-				); ?>
-			<?php if ( 'container' == $container ) : ?>
-			</div><!-- .container -->
-			<?php endif; ?>
-
-		</nav><!-- .site-navigation -->
-
-	</div><!-- #wrapper-navbar end -->
+	</div><!-- #wrapper-hero end -->

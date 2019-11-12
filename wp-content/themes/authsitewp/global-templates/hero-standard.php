efe0d3b06e39d1bc8cf8edc7820649887b4dacfe
Andy Bradbury <andy@thestartupguys.co.uk>
2019-11-12 02:05:33 +0000

    move the_post headings for single pages to the header file

--- wp-content/themes/authsitewp/global-templates/hero-standard.php
+++ wp-content/themes/authsitewp/global-templates/hero-standard.php
@@ -12,8 +12,12 @@ defined( 'ABSPATH' ) || exit;
 <div class="container hero-info" id="wrapper-hero-info">
     <div class="row justify-content-center">
         <div class="col-md-10">
-            <h1>.......</h1>
-            <span class="hero-description">.......</span>
+            <?php if ( ! is_home()) : ?>
+                <?php while ( have_posts() ) : the_post(); ?>
+		            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                    <span class="hero-description"><?php understrap_posted_on(); ?></span>
+                <?php endwhile; // end of the loop. ?>
+            <?php endif; ?>
         </div>
     </div>
 </div>
--- wp-content/themes/authsitewp/single.php
+++ wp-content/themes/authsitewp/single.php
@@ -17,31 +17,26 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
 		<div class="row">
+            <div class="col-lg-6 offset-lg-3">
+                <main class="site-main" id="main">
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
 
-			<main class="site-main" id="main">
+                        <?php get_template_part( 'loop-templates/content', 'single' ); ?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
+                        <?php understrap_post_nav(); ?>
 
-					<?php get_template_part( 'loop-templates/content', 'single' ); ?>
+                        <?php
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if ( comments_open() || get_comments_number() ) :
+                            comments_template();
+                        endif;
+                        ?>
 
-					<?php understrap_post_nav(); ?>
+                    <?php endwhile; // end of the loop. ?>
 
-					<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-					?>
-
-				<?php endwhile; // end of the loop. ?>
-
-			</main><!-- #main -->
-
-			<!-- Do the right sidebar check -->
-			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+                </main><!-- #main -->
+            </div>
 
 		</div><!-- .row -->
 
